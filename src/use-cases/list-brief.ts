0224b1184a15f9e93bc5a118d6d6cb7a5a0b4788
ItaloCabral <g1brunoyokima@hotmail.com>
2024-03-22 06:55:23 -0300

    feat(use-cases): list brief use case

---
+++ src/use-cases/list-brief.ts
@@ -0,0 +1,30 @@
+import { Brief } from "src/entities/brief";
+import { BriefRepository } from "src/repositories/brief-repositories/repository";
+
+type ListBriefRequest = {
+
+}
+
+export type ListBriefResponse = {
+    data: {
+        briefs: Brief[]
+    }
+}
+
+export class ListBriefUseCase {
+
+    public constructor(
+        private repository: BriefRepository
+    ) {}
+
+    async execute(): Promise<ListBriefResponse> {
+
+        const briefs = await this.repository.list();
+
+        return {
+            data: {
+                briefs
+            }
+        }
+    }
+}
