5ee161909ae54dc81fa1ffd9e46d03a55a2eb699
ItaloCabral <g1brunoyokima@hotmail.com>
2024-03-22 06:55:49 -0300

    feat(use-cases): retrieve brief (by id) use case

---
+++ src/use-cases/retrieve-brief.ts
@@ -0,0 +1,28 @@
+import { Brief } from "src/entities/brief";
+import { BriefRepository } from "src/repositories/brief-repositories/repository";
+
+type RetrieveBriefRequest = {
+    id: string;
+}
+
+type RetrieveBriefResponse = Brief;
+
+export class RetrieveBriefUseCase {
+
+    public constructor(
+        private repository: BriefRepository
+    ) {}
+
+    async execute({ id }: RetrieveBriefRequest): Promise<RetrieveBriefResponse> {
+
+        if(!id) throw new Error('id is required');
+
+        const brief = await this.repository.find(id);
+
+        if(!brief) throw new Error('Brief not found');
+
+        return brief;
+
+    }
+
+}
