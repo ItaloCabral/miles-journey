6a389c2cac256c8d0257502378440f74f577b76a
ItaloCabral <g1brunoyokima@hotmail.com>
2024-03-22 06:55:05 -0300

    feat(use-cases): crete brief use case

---
+++ src/use-cases/create-brief.ts
@@ -0,0 +1,44 @@
+import { Brief } from "src/entities/brief";
+import { BriefRepository } from "src/repositories/brief-repositories/repository";
+
+type CreateBriefRequest = {
+    imageUrl: string;
+    description: string;
+    author: string;
+}
+
+type CreateBriefResponse = {
+    id: string;
+}
+
+export class CreateBriefUseCase {
+
+    public constructor(
+        private repository: BriefRepository
+    ) {}
+
+    async execute({ imageUrl, description, author }: CreateBriefRequest): Promise<CreateBriefResponse> {
+
+        if (!imageUrl) throw new Error('imageUrl is required');
+
+        if (!description) throw new Error('description is required');
+
+        if (!author) throw new Error('author is required');
+
+        const brief = new Brief({
+            imageUrl,
+            description,
+            author
+        });
+
+        const savedBrief = await this.repository.save(brief);
+
+        if(!savedBrief) throw new Error('Error saving brief');
+
+        return {
+            id: savedBrief.id!
+        };
+
+    }
+
+}
