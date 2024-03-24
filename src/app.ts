a49125b6654c98cf901b40c331637929a56dcfea
ItaloCabral <g1brunoyokima@hotmail.com>
2024-03-24 02:37:20 -0300

    feat(app): enabling cors with app url env to permit requests from front end

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import cors from "cors";
+
 import { briefRoutes } from './routes/brief-routes';
+import { env } from './env';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/briefs', briefRoutes); 
+app.use(cors({
+    origin: env.APP_URL
+}));
+
+app.use('/api', briefRoutes);
 
 export {
     app as App
--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const envSchema = z.object({
   PORT: z.string().default('3000'),
   NODE_ENV: z.string().default('development'),
+  APP_URL: z.string().default('http://127.0.0.1'),
 })
 
 export type Env = z.infer<typeof envSchema>
